9ff50d138f73c1055117d68a2c203aad9625acb3
Thomas6A <thomas.baudrin@outlook.fr>
2022-11-30 14:27:26 +0100

    biblio

--- Bibliothèque/src/bibliothèque/Bibliothèque.java
+++ Bibliothèque/src/bibliothèque/Bibliothèque.java
@@ -1,11 +1,16 @@
 package bibliothèque;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Bibliothèque {
 	//Attributs
 	private ArrayList<Livre> Livres;
 	private ArrayList<String> Liste = new ArrayList<String>();
+	private Livre livre;
 	
 	//Constructeurs
 	public Bibliothèque(final ArrayList<Livre> Livres) {
@@ -15,15 +20,54 @@ public class Bibliothèque {
 	//Méthodes
 	public ArrayList<String> Liste_livres(){
 		
-		for (int i = 0; i < Livres.size();i++) {
+		for (int i = 0; i < this.Livres.size();i++) {
 			Liste.add(this.Livres.get(i).getTitre());
 		}
 		return Liste;
 	}
 	
+	public Livre detail_livre(Scanner input) {
+		System.out.println("Entrez le titre du livre dont vous voulez le détail");
+		String titre = input.nextLine();
+		for (int i = 0; i < this.Livres.size(); i++) {
+			if(titre.contains(this.Livres.get(i).getTitre())) {
+				livre = this.Livres.get(i);
+			}
+		}
+		return livre;
+	}
+	
+	public void export_fichier() {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter("biblio.csv"));
+			for(Livre livre:Livres) {
+				writer.write(livre.getTitre()+","+livre.getAuteur()+","+livre.getGenre()+","+livre.getNb_page()+","+livre.getNb_exemplaires()+"\n");
+			}
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
 	
 	
 	//Setters Getters
+
+	public ArrayList<String> getListe() {
+		return Liste;
+	}
+
+	public void setListe(ArrayList<String> liste) {
+		Liste = liste;
+	}
+
+	public Livre getLivre() {
+		return livre;
+	}
+
+	public void setLivre(Livre livre) {
+		this.livre = livre;
+	}
 
 	public ArrayList<Livre> getLivres() {
 		return Livres;
--- Bibliothèque/src/bibliothèque/Brief_Biblio.java
+++ Bibliothèque/src/bibliothèque/Brief_Biblio.java
@@ -1,5 +1,9 @@
 package bibliothèque;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -7,19 +11,32 @@ public class Brief_Biblio {
 	public static void main(String[] args) {
 		Scanner input = new Scanner (System.in);
 		ArrayList<Livre> Livres = new ArrayList<Livre>();
+		import_CSV(Livres);
 		Bibliothèque biblio = new Bibliothèque(Livres);
 		do {
 			int selection = Choix_util(input);
 			if(selection == 1) {
 				Choix1(Livres, input);
 			    biblio = new Bibliothèque(Livres);
-				continue;
+			    continue;
 			}else if(selection == 2) {
 				Choix2(Livres, input);
 				biblio = new Bibliothèque(Livres);
 				continue;
 			}else if(selection == 3) {
 				System.out.println(biblio.Liste_livres());
+				continue;
+			}else if(selection == 4) {
+				Livre livre = biblio.detail_livre(input);
+				System.out.println("L'auteur de "+ livre.getTitre() +" est : "+livre.getAuteur()+", son genre est : "+livre.getGenre()
+				+", son nombre de page est : "+livre.getNb_page()+" et son nombre d'exemplaire est :"+livre.getNb_exemplaires());
+				continue;
+			}else if(selection == 5) {
+				biblio.export_fichier();
+				break;
+			}else {
+				System.out.println("Veuillez rentrez un des choix disponibles.");
+				continue;
 			}
 			
 		}while(true);
@@ -59,7 +76,7 @@ public class Brief_Biblio {
 		input.nextLine();
 		String titre = input.nextLine();
 		for(int i = 0; i < Livres.size(); i++) {
-			if(titre == Livres.get(i).getTitre()) {
+			if(titre.contains(Livres.get(i).getTitre())){
 				System.out.println("Entrez l'auteur du livre");
 				Livres.get(i).setAuteur(input.nextLine());
 				System.out.println("Entrez le genre du livre");
@@ -73,5 +90,31 @@ public class Brief_Biblio {
 		}
 		
 	}
+	public static void import_CSV(ArrayList<Livre> Livres) {
+		String line = "";
+		String splitBy = ",";
+			try {
+				BufferedReader br = new BufferedReader(new FileReader("biblio.csv"));
+				while((line = br.readLine()) != null) {
+					String[] in = line.split(splitBy);
+					String titre = in[0];
+					String auteur = in[1];
+					String genre = in[2];
+					String page = in[3];
+					String exemplaire = in[4];
+					int nb_page = Integer.valueOf(page);
+					int nb_exemplaire = Integer.valueOf(exemplaire);
+					Livre livre = new Livre(titre, auteur, genre, nb_page, nb_exemplaire);
+					Livres.add(livre);
+				}
+				br.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		
+	}
+		
 
 }
